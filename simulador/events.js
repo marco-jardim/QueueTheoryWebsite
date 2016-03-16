f46230d477bd3d55ea54dc43fde2a0f45638ba66
Marco Jardim <marquinho.jardim@gmail.com>
2016-03-15 21:04:31 -0300

    Adding Random.js Lib

--- simulador/events.js
+++ simulador/events.js
@@ -1,34 +1,15 @@
 
-var myrand = {
-  random: {
-    // random exponential function from https://gist.github.com/nicolashery/5885280
-    exponential: function(rate, randomUniform) {
-      rate = rate || 1;
-
-      // Allow to pass a random uniform value or function
-      // Default to Math.random()
-      var U = randomUniform;
-      if (typeof randomUniform === 'function') U = randomUniform();
-      if (!U) U = Math.random();
-
-      return -Math.log(U)/rate;
-    }
-  }
-}
-
-function getUniformRandom(min, max) {
-  return (Math.random() * (max - min)) + min;
-}
-
 function geraListaEventos(MAX_EVENTOS, taxaChegada1, tipoSaida1, taxaSaida1, taxaChegada2, tipoSaida2, taxaSaida2) {
     prox_evento = ""
     lista_eventos = []
+
+     var randomNumbersGenerator = new Random();
     
     //###1 Saída Exponencial (Cenário 1)###
     if (tipoSaida1 == "exponencial" && tipoSaida2 == null) 
         for (k = 0; k < MAX_EVENTOS; k++) {
-            tempo_chegada1 = myrand.random.exponential(taxaChegada1) //#Pega um tempo de chegada
-            tempo_saida1 = myrand.random.exponential(taxaSaida1) //#Pega um tempo de saída
+            tempo_chegada1 = randomNumbersGenerator.exponential(taxaChegada1) //#Pega um tempo de chegada
+            tempo_saida1 = randomNumbersGenerator.exponential(taxaSaida1) //#Pega um tempo de saída
             
             //#Confere qual tempo é menor e atribui se o próximo evento  é chegada ou saída
             if (tempo_chegada1 < tempo_saida1)
@@ -43,10 +24,10 @@ function geraListaEventos(MAX_EVENTOS, taxaChegada1, tipoSaida1, taxaSaida1, tax
     if (tipoSaida1 == "exponencial" && tipoSaida2 == "exponencial")
         for (k = 0; k < MAX_EVENTOS; k++) {          
             //console.log(sistema_tipo1+sistema_tipo2, lista_eventos[-1])
-            tempo_chegada1 = myrand.random.exponential(taxaChegada1)
-            tempo_chegada2 = myrand.random.exponential(taxaChegada2)
-            tempo_saida1 = myrand.random.exponential(taxaSaida1)
-            tempo_saida2 = myrand.random.exponential(taxaSaida2)
+            tempo_chegada1 = randomNumbersGenerator.exponential(taxaChegada1)
+            tempo_chegada2 = randomNumbersGenerator.exponential(taxaChegada2)
+            tempo_saida1 = randomNumbersGenerator.exponential(taxaSaida1)
+            tempo_saida2 = randomNumbersGenerator.exponential(taxaSaida2)
             
             menor_tempo = Math.min(tempo_chegada1, tempo_chegada2, tempo_saida1, tempo_saida2)
                     
@@ -69,8 +50,8 @@ function geraListaEventos(MAX_EVENTOS, taxaChegada1, tipoSaida1, taxaSaida1, tax
         tempo_saida1 = 1.0/(taxaSaida1)
         tempo_saida2 = 1.0/(taxaSaida2)
         for (k = 0; k < MAX_EVENTOS; k++) {
-            tempo_chegada1 = myrand.random.exponential(taxaChegada1)
-            tempo_chegada2 = myrand.random.exponential(taxaChegada2)
+            tempo_chegada1 = randomNumbersGenerator.exponential(taxaChegada1)
+            tempo_chegada2 = randomNumbersGenerator.exponential(taxaChegada2)
             
             menor_tempo = Math.min(tempo_chegada1, tempo_chegada2, tempo_saida1, tempo_saida2)
             
@@ -102,11 +83,11 @@ function geraListaEventos(MAX_EVENTOS, taxaChegada1, tipoSaida1, taxaSaida1, tax
     //###2 Saídas Uniformes (Cenário 4)###
     if (tipoSaida1 == "uniforme" && tipoSaida2 == "uniforme") {
     //#Aqui deve-se considerar o tempo residual de atendimento de um cliente no servidor
-        tempo_saida1 = getUniformRandom(taxaSaida1[0], taxaSaida1[1])
-        tempo_saida2 = getUniformRandom(taxaSaida2[0], taxaSaida2[1])
+        tempo_saida1 = randomNumbersGenerator.uniform(taxaSaida1[0], taxaSaida1[1])
+        tempo_saida2 = randomNumbersGenerator.uniform(taxaSaida2[0], taxaSaida2[1])
         for (k = 0; k < MAX_EVENTOS; k++) {
-            tempo_chegada1 = myrand.random.exponential(taxaChegada1)
-            tempo_chegada2 = myrand.random.exponential(taxaChegada2)
+            tempo_chegada1 = randomNumbersGenerator.exponential(taxaChegada1)
+            tempo_chegada2 = randomNumbersGenerator.exponential(taxaChegada2)
             
             menor_tempo = Math.min(tempo_chegada1, tempo_chegada2, tempo_saida1, tempo_saida2)
             
@@ -122,11 +103,11 @@ function geraListaEventos(MAX_EVENTOS, taxaChegada1, tipoSaida1, taxaSaida1, tax
             } else if (menor_tempo == tempo_saida1) {
                 prox_evento = "saida1"
                 tempo_saida2 = (tempo_saida2-tempo_saida1)
-                tempo_saida1 = getUniformRandom(taxaSaida1[0], taxaSaida1[1])
+                tempo_saida1 = randomNumbersGenerator.uniform(taxaSaida1[0], taxaSaida1[1])
             } else {
                 prox_evento = "saida2"
                 tempo_saida1 = (tempo_saida1-tempo_saida1)
-                tempo_saida2 = getUniformRandom(taxaSaida2[0], taxaSaida2[1])
+                tempo_saida2 = randomNumbersGenerator.uniform(taxaSaida2[0], taxaSaida2[1])
             }
                     
             lista_eventos.push(prox_evento)
