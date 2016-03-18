3aa4de67af9615c3b9715e9e15bbe59c5e38dd5f
Mario Cecchi <macecchi@gmail.com>
2016-03-18 01:05:00 -0300

    questão 8 cenário 3

--- js/graphs.js
+++ js/graphs.js
@@ -21,36 +21,36 @@ var graphs = {
                 }
             },
             series: [ 
-                {
-                    name: 'Analítico, sem prioridade',
-                    type: 'line',
-                    data: []
-                },
-                {
-                    name: 'Analítico, com prioridade, sem preemp.',
-                    type: 'line',
-                    data: []
-                },
-                {
-                    name: 'Analítico, com prioridade, com preemp.',
-                    type: 'line',
-                    data: []
-                },
-                {
-                    name: 'Simulação, sem prioridade',
-                    type: 'line',
-                    data: []
-                },
-                {
-                    name: 'Simulação, com prioridade, sem preemp.',
-                    type: 'line',
-                    data: []
-                },
-                {
-                    name: 'Simulação, com prioridade, com preemp.',
-                    type: 'line',
-                    data: []
-                }
+            {
+                name: 'Analítico, sem prioridade',
+                type: 'line',
+                data: []
+            },
+            {
+                name: 'Analítico, com prioridade, sem preemp.',
+                type: 'line',
+                data: []
+            },
+            {
+                name: 'Analítico, com prioridade, com preemp.',
+                type: 'line',
+                data: []
+            },
+            {
+                name: 'Simulação, sem prioridade',
+                type: 'line',
+                data: []
+            },
+            {
+                name: 'Simulação, com prioridade, sem preemp.',
+                type: 'line',
+                data: []
+            },
+            {
+                name: 'Simulação, com prioridade, com preemp.',
+                type: 'line',
+                data: []
+            }
             ]
         });
         $('#'+element).highcharts();
@@ -79,36 +79,36 @@ var graphs = {
                 }
             },
             series: [ 
-                {
-                    name: 'Analítico, sem prioridade',
-                    type: 'line',
-                    data: []
-                },
-                {
-                    name: 'Analítico, com prioridade, sem preemp.',
-                    type: 'line',
-                    data: []
-                },
-                {
-                    name: 'Analítico, com prioridade, com preemp.',
-                    type: 'line',
-                    data: []
-                },
-                {
-                    name: 'Simulação, sem prioridade',
-                    type: 'line',
-                    data: []
-                },
-                {
-                    name: 'Simulação, com prioridade, sem preemp.',
-                    type: 'line',
-                    data: []
-                },
-                {
-                    name: 'Simulação, com prioridade, com preemp.',
-                    type: 'line',
-                    data: []
-                }
+            {
+                name: 'Analítico, sem prioridade',
+                type: 'line',
+                data: []
+            },
+            {
+                name: 'Analítico, com prioridade, sem preemp.',
+                type: 'line',
+                data: []
+            },
+            {
+                name: 'Analítico, com prioridade, com preemp.',
+                type: 'line',
+                data: []
+            },
+            {
+                name: 'Simulação, sem prioridade',
+                type: 'line',
+                data: []
+            },
+            {
+                name: 'Simulação, com prioridade, sem preemp.',
+                type: 'line',
+                data: []
+            },
+            {
+                name: 'Simulação, com prioridade, com preemp.',
+                type: 'line',
+                data: []
+            }
             ]
         });
         $('#'+element).highcharts();
@@ -138,22 +138,116 @@ var graphs = {
                 }
             },
             series: [ 
-                {
-                    name: 'Sistema está vazio',
-                    data: []
-                },
-                {
-                    name: 'Chegadas encontram o sistema vazio',
-                    data: []
-                },
-                {
-                    name: 'Sistema está vazio (chegadas tempo determ.)',
-                    data: []
-                },
-                {
-                    name: 'Chegadas encontram o sistema vazio (chegadas tempo determ.)',
-                    data: []
+            {
+                name: 'Sistema está vazio',
+                data: []
+            },
+            {
+                name: 'Chegadas encontram o sistema vazio',
+                data: []
+            },
+            {
+                name: 'Sistema está vazio (chegadas tempo determ.)',
+                data: []
+            },
+            {
+                name: 'Chegadas encontram o sistema vazio (chegadas tempo determ.)',
+                data: []
+            }
+            ]
+        });
+        $('#'+element).highcharts();
+        return chart;
+    },
+
+    LambdaVsPendingWork: function(lambdaData, element) {
+        element = element || 'averagePendingWorkChart';
+        chart = new Highcharts.Chart({
+            chart: {
+                type: 'column',
+                renderTo: element,
+                shadow: true
+            },
+            title: {
+                text: 'Lambda vs. Trabalho pendente médio'
+            },
+            xAxis: {
+                categories: lambdaData,
+                title:{
+                    text:'λ (λ1 + λ2)'
                 }
+            },
+            yAxis: {
+                title: {
+                    text: 'Trabalho pendente médio'
+                }
+            },
+            series: [ 
+            {
+                name: 'Trab. pendente, sem prioridade',
+                data: []
+            },
+            {
+                name: 'Trab. pendente, com prioridade, sem preemp.',
+                data: []
+            },
+            {
+                name: 'Trab. pendente, com prioridade, com preemp.',
+                data: []
+            }
+            ]
+        });
+        $('#'+element).highcharts();
+        return chart;
+    },
+
+    LambdaVsCustomersEachQueue: function(lambdaData, element) {
+        element = element || 'averageCustomersEachQueueChart';
+        chart = new Highcharts.Chart({
+            chart: {
+                type: 'column',
+                renderTo: element,
+                shadow: true
+            },
+            title: {
+                text: 'Lambda vs. Trabalho pendente médio'
+            },
+            xAxis: {
+                categories: lambdaData,
+                title:{
+                    text:'λ (λ1 + λ2)'
+                }
+            },
+            yAxis: {
+                title: {
+                    text: 'Trabalho pendente médio'
+                }
+            },
+            series: [ 
+            {
+                name: 'Nº clientes fila 1, sem prioridade',
+                data: []
+            },
+            {
+                name: 'Nº clientes fila 2, sem prioridade',
+                data: []
+            },
+            {
+                name: 'Nº clientes fila 1, sem preemp.',
+                data: []
+            },
+            {
+                name: 'Nº clientes fila 2, sem preemp.',
+                data: []
+            },
+            {
+                name: 'Nº clientes fila 1, com preemp.',
+                data: []
+            },
+            {
+                name: 'Nº clientes fila 2, com preemp.',
+                data: []
+            },
             ]
         });
         $('#'+element).highcharts();
